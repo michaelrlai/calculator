de012f37eb80cfdfbcd2ddc3ac66affd03ec02a8
Michael Lai <mrlai7782@gmail.com>
2021-01-15 02:12:12 -0800

    Almost done

--- script.js
+++ script.js
@@ -15,31 +15,97 @@ function divide(a, b) {
 }
 
 function operate(operator, num1, num2) {
-    let answer;
-    if (operator === '+') {
+    num1 = Number(num1);
+    num2 = Number(num2);
+
+    if (operator === 'add') {
         answer = add(num1, num2);
-    } else if (operator === '-') {
+    } else if (operator === 'subtract') {
         answer = subtract(num1, num2);
-    } else if (operator === '*') {
+    } else if (operator === 'multiply') {
         answer = multiply(num1, num2);
-    } else if (operator === '/') {
+    } else if (operator === 'divide') {
         answer = divide(num1, num2);
     }
-    return answer;
+    console.log('answer: ' + answer);
+    return Math.round(answer * 100000) / 100000;
 }
 
 function input() {
-    this.classList.add('highlight');
+  //  this.classList.add('highlight'); // Adds highlight to button press
 
-    if (value === 0) {
+    if ((value === '0' || value === 0) 
+            && this.id !== 'divide' 
+            && this.id !== 'multiply' 
+            && this.id !== 'subtract' 
+            && this.id !== 'add' 
+            && this.id !== 'equals' ){
         value = '';
     }
     if (this.id === 'clear') {
-        value = 0;
+        consecEquals = 0;
+        clearAfterEquals = 0;
+        answer = '';
+        haveFirst = 0;
+        clearNext = 0;
+        num1 = '';
+        num2 = '';
+        operator = '';
+        value = '0';
+       // haveFirst = 0;
+    } else if (this.id === 'divide' || this.id === 'multiply' || this.id === 'subtract' || this.id === 'add') {
+        operator = this.id;
+        num1 = value;
+        num2 = '';
+        clearNext = 1;
+        consecEquals = 0;
+      //  haveFirst = 1;
+    } else if (this.id === 'equals') {
+        if (consecEquals !== 1 && num1 !== '') {
+            num2 = value;
+        }
+        if (num1 === '' && num2 === ''){
+            console.log('YES');
+            
+        } else {
+            answer = operate (operator, num1, num2);
+            value = answer;
+            num1 = answer;
+            clearAfterEquals = 1;
+            consecEquals = 1;
+        }
+    } else if (this.id === 'decimal') {
+        if (clearNext === 1) {
+            value = '0';
+            clearNext = 0;
+        }
+        if (value === '') {
+            value = 0;
+        } else if (clearAfterEquals === 1) {
+            value = '0';
+            clearAfterEquals = 0;
+        }
+        test = value.toString();
+        if (!test.includes('.')) {
+        consecEquals = 0;
+        value += this.textContent;
+        }
     } else {
+        if (clearNext === 1) {
+            value = '';
+            clearNext = 0;
+        
+        } else if (clearAfterEquals === 1) {
+            value = '';
+            clearAfterEquals = 0;
+        }
+        consecEquals = 0;
         value += this.textContent;
     }
+
     displaying.textContent = value;
+
+    console.log('num1: ' + num1, 'num2: ' + num2, 'operator:' + operator);
 }
 
 function removeHighlight (e) {
@@ -47,17 +113,24 @@ function removeHighlight (e) {
     this.classList.remove('highlight');
 }
 
+let consecEquals = 0;
+let clearAfterEquals = 0;
+let answer = '';
+let haveFirst = 0;
+let clearNext = 0;
+let num1 = '';
+let num2 = '';
+let operator = '';
+let value = '0';
 
-
-let value = 0;
 let displaying = document.querySelector('#display');
 displaying.textContent = value;
 
 let button = document.querySelectorAll('.button');
 
-for (let i = 0; i < button.length - 1; i++) {
+for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('mousedown', input);
-    button[i].addEventListener('mouseup', removeHighlight);
+ //   button[i].addEventListener('mouseup', removeHighlight); // Removes highlight from button press
 }
 
-console.log(button);
+
