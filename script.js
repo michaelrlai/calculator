52c5fa56987122121a773186f07cf16d9bdb3b87
Michael Lai <mrlai7782@gmail.com>
2021-01-19 00:18:26 -0800

    Added highlighted buttons

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function add(a, b) {
     return a + b;
 }
-
 function subtract(a, b) {
     return a - b;
 }
-
 function multiply(a, b) {
     return a * b;
 }
-
 function divide(a, b) {
     return a / b;
 }
@@ -17,35 +14,22 @@ function divide(a, b) {
 function operate(operator, num1, num2) {
     num1 = Number(num1);
     num2 = Number(num2);
-
-    if (operator === 'add') {
-        answer = add(num1, num2);
-    } else if (operator === 'subtract') {
-        answer = subtract(num1, num2);
-    } else if (operator === 'multiply') {
-        answer = multiply(num1, num2);
-    } else if (operator === 'divide') {
-        answer = divide(num1, num2);
-    }
-    console.log('answer: ' + answer);
-    if (answer === Infinity || isNaN(answer) ) {
-        return 'oops';
-    } else {
-        return Math.round(answer * 100000) / 100000;
-    }
+    if (operator === 'add') answer = add(num1, num2);
+    else if (operator === 'subtract') answer = subtract(num1, num2);
+    else if (operator === 'multiply') answer = multiply(num1, num2);
+    else if (operator === 'divide') answer = divide(num1, num2);
+    if (answer === Infinity || isNaN(answer) ) return 'oops';
+    else return Math.round(answer * 100000) / 100000;
 }
 
-
 function input(key) {
-  //  this.classList.add('highlight'); // Adds highlight to button press
-
-  
     if ((value === '0' || value === 0) 
-            && this.id !== 'divide' 
-            && this.id !== 'multiply' 
-            && this.id !== 'subtract' 
-            && this.id !== 'add' 
-            && this.id !== 'equals' ){
+            && key !== '+' && this.id !== 'add' 
+            && key !== '-' && this.id !== 'subtract' 
+            && key !== '*' && this.id !== 'multiply' 
+            && key !== '/' && this.id !== 'divide'
+            && key !== '=' && this.id !== 'equals'
+            && key !== '.' && this.id !== 'decimal') {
         value = '';
     }
     if (key === ' ' || this.id === 'clear') {
@@ -59,8 +43,10 @@ function input(key) {
         operator = '';
         value = '0';
         consecOperator = 0;
-       // haveFirst = 0;
-    } else if (key === '+' || this.id === 'add') {
+    } if (key === '+' || this.id === 'add' 
+            || key === '-' || this.id === 'subtract' 
+            || key === '*' || this.id === 'multiply' 
+            || key === '/' || this.id === 'divide') {
         if (num1 !== '' && num2 === '' && consecOperator !== 1) {
             num2 = value;
             answer = operate (operator, num1, num2);
@@ -69,92 +55,29 @@ function input(key) {
             num2 = '';
             clearAfterEquals = 1;
             consecEquals = 0;
+            consecOperator = 1;
+        } else if (consecOperator !== 1) {
+            num1 = value;
+            num2 = '';
+            clearNext = 1;
+            consecEquals = 0;
+            clearAfterEquals = 0;
+            consecOperator = 1;
+        } 
+        if (key === '+' || this.id === 'add') {
             operator = 'add';
-            consecOperator = 1;
-        } else if (consecOperator !== 1) {
-            operator = 'add';
-            num1 = value;
-            num2 = '';
-            clearNext = 1;
-            consecEquals = 0;
-            clearAfterEquals = 0;
-            consecOperator = 1;
-        }  else if (consecOperator === 1) {
-            operator = 'add';
-        }
-    } else if (key === '-' || this.id === 'subtract') {
-        if (num1 !== '' && num2 === '' && consecOperator !== 1) {
-            num2 = value;
-            answer = operate (operator, num1, num2);
-            value = answer;
-            num1 = answer;
-            num2 = '';
-            clearAfterEquals = 1;
-            consecEquals = 0;
+        } else if (key === '-' || this.id === 'subtract') {
             operator = 'subtract';
-            consecOperator = 1;
-        } else if (consecOperator !== 1) {
-            operator = 'subtract';
-            num1 = value;
-            num2 = '';
-            clearNext = 1;
-            consecEquals = 0;
-            clearAfterEquals = 0;
-            consecOperator = 1;
-        } else if (consecOperator === 1) {
-            operator = 'subtract';
-        }
-    } else if (key === '*' || this.id === 'multiply') {
-        if (num1 !== '' && num2 === '' && consecOperator !== 1) {
-            num2 = value;
-            answer = operate (operator, num1, num2);
-            value = answer;
-            num1 = answer;
-            num2 = '';
-            clearAfterEquals = 1;
-            consecEquals = 0;
+        } else if (key === '*' || this.id === 'multiply') {
             operator = 'multiply';
-            consecOperator = 1;
-        } else if (consecOperator !== 1) {
-            operator = 'multiply';
-            num1 = value;
-            num2 = '';
-            clearNext = 1;
-            consecEquals = 0;
-            clearAfterEquals = 0;
-            consecOperator = 1;
-        } else if (consecOperator === 1) {
-            operator = 'multiply';
-            console.log('UYES');
-        }
-    } else if (key === '/' || this.id === 'divide') {
-        if (num1 !== '' && num2 === '' && consecOperator !== 1) {
-            num2 = value;
-            answer = operate (operator, num1, num2);
-            value = answer;
-            num1 = answer;
-            num2 = '';
-            clearAfterEquals = 1;
-            consecEquals = 0;
-            operator = 'divide';
-            consecOperator = 1;
-        } else if (consecOperator !== 1) {
-            operator = 'divide';
-            num1 = value;
-            num2 = '';
-            clearNext = 1;
-            consecEquals = 0;
-            clearAfterEquals = 0;
-            consecOperator = 1;
-        } else if (consecOperator === 1) {
+        }  else if (key === '/' || this.id === 'divide') {
             operator = 'divide';
         }
     } else if (key === 'Enter' || key === '=' || this.id === 'equals') {
         if (consecEquals !== 1 && num1 !== '') {
             num2 = value;
         }
-        if (num1 === '' && num2 === ''){
-        } else {
+        if (num1 !== '' && num2 !== ''){
             answer = operate (operator, num1, num2);
             value = answer;
             num1 = answer;
@@ -167,9 +90,7 @@ function input(key) {
             value = '0';
             clearNext = 0;
         }
-        if (value === '') {
-            value = 0;
-        } else if (clearAfterEquals === 1) {
+        if (clearAfterEquals === 1) {
             value = '0';
             clearAfterEquals = 0;
         }
@@ -183,22 +104,18 @@ function input(key) {
         if (clearAfterEquals === 0) {
             value = value.slice(0, -1);
         }
-        if (value === '')
-        {
-            value = 0;
+        if (value === '') {
+            value = '0';
         }
         consecOperator = 0;
     } else {
         if (clearNext === 1) {
             value = '';
             clearNext = 0;
-        
         } else if (clearAfterEquals === 1) {
             value = '';
             clearAfterEquals = 0;
         }
-        consecEquals = 0;
-        consecOperator = 0;
         if (key === '0' || this.id === 'zero') value += 0;
         if (key === '1' || this.id === 'one') value += 1;
         if (key === '2' || this.id === 'two') value += 2;
@@ -209,16 +126,15 @@ function input(key) {
         if (key === '7' || this.id === 'seven') value += 7;
         if (key === '8' || this.id === 'eight') value += 8;
         if (key === '9' || this.id === 'nine') value += 9;
+        consecEquals = 0;
+        consecOperator = 0;
     }
-    
     displaying.textContent = value;
-
- //   console.log('value: ' + value,'num1: ' + num1, 'num2: ' + num2, 'operator: ' + operator);
-
+/*     console.log('value: ' + value,'num1: ' + num1, 'num2: ' + num2, 'operator: ' + operator); // For debugging
+    console.log('consecEquals: ' + consecEquals); */
 }
 
 function keypress (key) {
-
     if (
         key.key === '1'
         || key.key === '2'
@@ -242,13 +158,7 @@ function keypress (key) {
         ) {
         input(key.key);
     }
-
 }
-
-/* function removeHighlight (e) {
-
-    this.classList.remove('highlight');
-} */
 
 let consecOperator = 0;
 let consecEquals = 0;
@@ -263,12 +173,9 @@ let value = '0';
 
 let displaying = document.querySelector('#display');
 displaying.textContent = value;
-
 let button = document.querySelectorAll('.button');
 
 for (let i = 0; i < button.length; i++) {
     button[i].addEventListener('mousedown', input);
- //   button[i].addEventListener('mouseup', removeHighlight); // Removes highlight from button press
 }
-
 document.addEventListener('keydown', keypress);
